b66e8c3c0ce7e53abd921ebc6698fdc386a6934a
Me Priyank <pvonlyone29@gmail.com>
2024-09-13 23:54:13 +0530

    counter

--- react-learning/src/App.jsx
+++ react-learning/src/App.jsx
@@ -5,16 +5,24 @@ import {useState} from "react"
 function App() {
   const [count , setCount] = useState(0);
 
-  function press(){
-    setCount(count+1);
-  }
+
 
   return (
     <div>
-      <button onClick = {press}>Counter {count}</button>
+      <CustomBtn count = {count}  setCount = {setCount}></CustomBtn>
     </div>
   )
      
 }
+
+function CustomBtn(props){
+  function press(){
+    props.setCount(props.count+1);
+  }
+
+return <button onClick={press}>Counter {props.count}</button>
+   
+  }
+
 
 export default App
