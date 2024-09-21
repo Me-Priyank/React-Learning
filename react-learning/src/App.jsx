ae0088640f61e37db5fbdb1c4355075dbddf9e51
Me Priyank <pvonlyone29@gmail.com>
2024-09-21 13:44:03 +0530

    More React

--- react-learning/src/App.jsx
+++ react-learning/src/App.jsx
@@ -1,10 +1,16 @@
 import { useState } from "react"
-
+import {memo} from "react" // To prevent unneccesary re-rendering of all components
 function App() {
+const [title,setTitle]= useState("hi , i'm Priyank");
+
+function addTodo(){
+  setTitle("hi , i'm" + Math.random());
+}
+
 
 return <div>
-  
-  <More></More>
+  <button onClick={addTodo}>c</button>
+  <Header title={title}></Header>
   <Header title="2"></Header>
   <Header title="2"></Header>
   <Header title="2"></Header>
@@ -14,25 +20,13 @@ return <div>
      
 }
 
-function More(){
-  const [title,setTitle]= useState("hi , i'm Priyank");
 
-  function addTodo(){
-    setTitle("hi , i'm" + Math.random());
-  }
-
-  return <div>
-      <button onClick={addTodo}>c</button>
-  <Header title={title}></Header>
-  </div>
-
-}
-
-function Header ({title}){
+const Header = memo( ({title})=>{
   return <div>
     {title}
   </div>
 }
+)
 
 
 
