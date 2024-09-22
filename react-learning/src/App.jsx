77cd64af734d7fefd6afa91b15d49a7f4982892a
Me Priyank <pvonlyone29@gmail.com>
2024-09-22 16:27:30 +0530

    react fundamentals

--- react-learning/src/App.jsx
+++ react-learning/src/App.jsx
@@ -1,33 +1,9 @@
-import { useState } from "react"
-import {memo} from "react" // To prevent unneccesary re-rendering of all components
+
 function App() {
-const [title,setTitle]= useState("hi , i'm Priyank");
-
-function addTodo(){
-  setTitle("hi , i'm" + Math.random());
-}
-
-
-return <div>
-  <button onClick={addTodo}>c</button>
-  <Header title={title}></Header>
-  <Header title="2"></Header>
-  <Header title="2"></Header>
-  <Header title="2"></Header>
-
-</div>
-
-     
-}
-
-
-const Header = memo( ({title})=>{
-  return <div>
-    {title}
-  </div>
-}
+return(
+  <h1>Hi</h1>
 )
-
+}
 
 
 
